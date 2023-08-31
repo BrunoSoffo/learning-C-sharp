d1887b545d6a38cbc50305fffc9ea660278dc8a6
BrunoSoffo <brussoffo@gmail.com>
2023-08-30 23:45:36 -0300

    Enum

---
+++ Enumeradores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MeuNamespace // Substitua "MeuNamespace" pelo nome desejado para o seu namespace
+{
+    enum DiasSemana{Domingo,Segunda,Terça,Quarta,Quinta,Sexta,Sábado};
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ids = (int)DiasSemana.Sexta; // esse comando atribuirá o indice de Sexta para a var ids
+
+            DiasSemana dss = (DiasSemana)3;
+            DiasSemana ds = DiasSemana.Domingo;
+
+            Console.WriteLine(ids);
+            Console.WriteLine(dss);
+            Console.WriteLine(ds);
+        }
+    }
+
+}
+
+
+
