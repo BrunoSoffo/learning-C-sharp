b042dba46a75e189882f49c45a31f409963f623b
BrunoSoffo <brussoffo@gmail.com>
2023-08-30 23:19:04 -0300

    Bitwise

---
+++ Bitwise/Program.cs
@@ -0,0 +1,8 @@
+﻿int num1 = 10;
+int num2 = 10;
+
+num1=num1<<1; // o valor após o "<<" & ">>" é a quantidade de vezes q o valor será alterado
+num2=num2>>1;
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
