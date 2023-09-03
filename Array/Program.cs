14579c61e082cbf571de30d86bc25d6ad66ae689
BrunoSoffo <brussoffo@gmail.com>
2023-09-02 23:56:39 -0300

    array

---
+++ Array/Program.cs
@@ -0,0 +1,18 @@
+﻿//formas de se criar um array
+int[] n1 = new int[5];
+int[] n2 = new int[3]{88,66,99};
+int[] n3={55,44,77,33};
+string[] veiculos = new string[3];
+
+veiculos[0]="Carro";
+veiculos[0]="Avião";
+veiculos[0]="Barco";
+
+n[0]=11;
+n[1]=22;
+n[2]=33;
+n[3]=44;
+n[4]=55;
+
+Console.WriteLine(n1[1]);
+Console.WriteLine(n2[1]);
