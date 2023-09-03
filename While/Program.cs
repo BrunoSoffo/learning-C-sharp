3aa35245dfb123c4336738a5c7560cbf9d733fb6
BrunoSoffo <brussoffo@gmail.com>
2023-09-03 00:28:04 -0300

    While

---
+++ While/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] num = new int[10];
+
+int i = num.Length-1; // precisamos usae o -1 pois sem ele, o indice do array irá de 0 a 10, totalizando 11 posições
+
+while(i>0){
+    num[i]=i;
+    Console.WriteLine(num[i]);
+    i--;
+}
