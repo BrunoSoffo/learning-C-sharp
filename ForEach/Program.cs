10875ec00a23fcd7afa12cefdb792e6059174927
BrunoSoffo <brussoffo@gmail.com>
2023-09-08 23:41:40 -0300

    ForEach

---
+++ ForEach/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] n = new int[3]{11,22,33};
+
+
+//lendo os elementos de uma lista com For
+for(int i=0; i<n.Length; i++){
+    Console.WriteLine(n[i]);
+}
+
+//lendo os elementos de uma lista com foreach
+//obs: o foreach n é indicado para inicializar, atribuir valores para um array, apenas para ler
+foreach(int j in n){ //a variável do for precisa ser do mesmo tipo da lista
+    Console.WriteLine(j);
+}
