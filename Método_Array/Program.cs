d0720ffb8234904c8a8c576436f643d25b613223
BrunoSoffo <brussoffo@gmail.com>
2023-09-09 23:52:35 -0300

    Métodos para Array (pt1)

---
+++ Método_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] v1=new int[5];
+int[] v2=new int[5];
+int[] v3=new int[5];
+int[] v4=new int[5];
+int[,] v1=new int[2,5]{{11,22,00,44,55},{66,77,88,99,00}};
+
+Random random=new Random();
+for(int i=0; i<v1.Length;i++){
+    v1[1]=random.Next(50);
+}
+
+Console.WriteLine("Elementos do vetor  1");
+foreach (int n in v1)
+{
+    Console.WriteLine(n);
+}
